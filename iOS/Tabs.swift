62c3828a4db0ce09e0b48a0b9236546ec77a40f5
vaux <zero.griffin@gmail.com>
2021-09-22 18:08:51 +0200

    Update tab

--- iOS/Tabs.swift
+++ iOS/Tabs.swift
@@ -1,10 +1,13 @@
 import SwiftUI
+import Combine
 
 struct Tabs: View {
     @Binding var status: [Navigation.Status]
     @State var transition: Transition?
     let tab: (Int) -> Void
+    @State private var entering = true
     @Namespace private var animating
+    private let scroll = PassthroughSubject<UUID, Never>()
     
     var body: some View {
         ZStack {
@@ -17,10 +20,12 @@ struct Tabs: View {
                 Snap(image: status[transition!.index].image, size: transition!.size)
                     .edgesIgnoringSafeArea(.all)
                     .allowsHitTesting(false)
-                    .matchedGeometryEffect(id: status[transition!.index].id, in: animating, properties: .position, isSource: true)
+                    .matchedGeometryEffect(id: status[transition!.index].id, in: animating, properties: .position, isSource: entering)
             }
         }
         .task {
+            scroll.send(status[transition!.index].id)
+            
             withAnimation(.easeInOut(duration: 0.3)) {
                 transition!.size = 150
             }
@@ -40,21 +45,28 @@ struct Tabs: View {
             .firstIndex {
                 $0.id == id
             }!
+        
+        entering = false
+        
+        withAnimation(.easeInOut(duration: 0.2)) {
+            scroll.send(id)
+        }
+        
         withAnimation(.easeInOut(duration: 0.2)) {
             transition = .init(size: 150, index: index)
         }
         
         DispatchQueue
             .main
-            .asyncAfter(deadline: .now() + 0.2) {
-                withAnimation(.easeInOut(duration: 0.35)) {
+            .asyncAfter(deadline: .now() + 0.1) {
+                withAnimation(.easeInOut(duration: 0.3)) {
                     transition?.size = nil
                 }
             }
         
         DispatchQueue
             .main
-            .asyncAfter(deadline: .now() + 0.55) {
+            .asyncAfter(deadline: .now() + 0.4) {
                 tab(index)
             }
     }
@@ -68,13 +80,9 @@ struct Tabs: View {
                         .frame(maxHeight: .greatestFiniteMagnitude)
                     ForEach(status) {
                         if transition != nil && $0.id == status[transition!.index].id {
-                            Item(status: $0)
-                                .matchedGeometryEffect(id: $0.id, in: animating, properties: .position, isSource: false)
-                                .id($0.id)
+                            item($0)
                         } else {
-                            Item(status: $0)
-                                .matchedGeometryEffect(id: $0.id, in: animating, properties: .position, isSource: false)
-                                .id($0.id)
+                            item($0)
                         }
                     }
                     Spacer()
@@ -83,8 +91,8 @@ struct Tabs: View {
                 }
             }
             .frame(maxWidth: .greatestFiniteMagnitude)
-            .onAppear {
-                proxy.scrollTo(status[transition!.index].id, anchor: .center)
+            .onReceive(scroll) {
+                proxy.scrollTo($0, anchor: .center)
             }
         }
     }
@@ -93,7 +101,12 @@ struct Tabs: View {
         Group {
             Button {
                 status.append(.init())
-                open(status.last!.id)
+                
+                DispatchQueue
+                    .main
+                    .asyncAfter(deadline: .now() + 0.1) {
+                        open(status.last!.id)
+                    }
             } label: {
                 Image(systemName: "plus.circle.fill")
                     .font(.largeTitle)
@@ -103,5 +116,14 @@ struct Tabs: View {
         }
         .frame(maxHeight: .greatestFiniteMagnitude, alignment: .bottom)
         .padding(.bottom)
+    }
+    
+    private func item(_ status: Navigation.Status) -> some View {
+        Snap(image: status.image, size: 150)
+            .matchedGeometryEffect(id: status.id, in: animating, properties: .position, isSource: !entering)
+            .id(status.id)
+            .onTapGesture {
+                open(status.id)
+            }
     }
 }
