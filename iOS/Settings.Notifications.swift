4c19bb4eb098ddbf9fb3f5bdd50550eb4e642ab8
vaux <zero.griffin@gmail.com>
2021-10-15 17:10:17 +0200

    Update settings

---
+++ iOS/Settings.Notifications.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+extension Settings {
+    struct Notifications: View {
+        @Binding var presented: Bool
+        @State private var requested = true
+        @State private var enabled = true
+        
+        var body: some View {
+            Section("Notifications") {
+                Text(enabled ? Copy.deactivate : Copy.notifications)
+                    .font(.footnote)
+                    .foregroundStyle(.secondary)
+                    .allowsHitTesting(false)
+                Action(title: enabled ? "Open Settings" : "Activate notifications", symbol: "app.badge") {
+                    if enabled || requested {
+                        presented = false
+                        UIApplication.shared.settings()
+                    } else {
+                        Task {
+                            _ = await UNUserNotificationCenter.request()
+                            requested = true
+                            await check()
+                        }
+                    }
+                }
+            }
+            .headerProminence(.increased)
+            .task {
+                await check()
+            }
+        }
+        
+        private func check() async {
+            let settings = await UNUserNotificationCenter.current().notificationSettings()
+            if settings.authorizationStatus == .notDetermined {
+                requested = false
+            } else if settings.alertSetting == .disabled {
+                enabled = false
+            }
+        }
+    }
+}
