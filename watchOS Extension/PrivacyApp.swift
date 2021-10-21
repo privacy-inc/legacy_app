f48160f81c412dc41a7c6392c170895b75f7dad9
vaux <zero.griffin@gmail.com>
2021-10-21 17:05:24 +0200

    Add watch

---
+++ watchOS Extension/PrivacyApp.swift
@@ -0,0 +1,19 @@
+//
+//  PrivacyApp.swift
+//  watchOS WatchKit Extension
+//
+//  Created by vaux on 21.10.21.
+//
+
+import SwiftUI
+
+@main
+struct PrivacyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
