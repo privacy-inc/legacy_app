e64bc630405f026bf39b0056cf89fa23249ed4a3
vaux <zero.griffin@gmail.com>
2021-11-11 13:06:40 +0100

    Add activity

--- macOS/Activity.Card.swift
+++ macOS/Activity.Card.swift
@@ -1,22 +1,40 @@
 import AppKit
+import Combine
 
 extension Activity {
     final class Card: NSView {
+        private var subs = Set<AnyCancellable>()
+        
         required init?(coder: NSCoder) { nil }
         init() {
             super.init(frame: .zero)
             translatesAutoresizingMaskIntoConstraints = false
-            layer = Layer()
+            let layer = Layer()
+            self.layer = layer
             wantsLayer = true
             shadow = NSShadow()
             shadow!.shadowColor = .init(white: 0, alpha: 0.1)
-            layer!.backgroundColor = NSColor.controlBackgroundColor.cgColor
-            layer!.cornerCurve = .continuous
-            layer!.cornerRadius = 14
-            layer!.shadowOffset = .zero
-            layer!.shadowRadius = 5
+            layer.backgroundColor = NSColor.controlBackgroundColor.cgColor
+            layer.cornerCurve = .continuous
+            layer.cornerRadius = 14
+            layer.shadowOffset = .zero
+            layer.shadowRadius = 5
             
-            widthAnchor.constraint(equalToConstant: 400).isActive = true
+            widthAnchor.constraint(equalToConstant: 300).isActive = true
+            
+            cloud
+                .map(\.events.stats.timeline)
+                .removeDuplicates()
+                .sink {
+                    layer
+                        .sublayers?
+                        .forEach {
+                            $0.removeFromSuperlayer()
+                        }
+                    
+                    layer.addSublayer(Chart(values: $0))
+                }
+                .store(in: &subs)
         }
         
         override func hitTest(_: NSPoint) -> NSView? {
--- macOS/Activity.swift
+++ macOS/Activity.swift
@@ -5,7 +5,7 @@ final class Activity: NSWindow {
     private var subs = Set<AnyCancellable>()
     
     init() {
-        super.init(contentRect: .init(x: 0, y: 0, width: 800, height: 400),
+        super.init(contentRect: .init(x: 0, y: 0, width: 1000, height: 300),
                    styleMask: [.closable, .titled, .fullSizeContentView], backing: .buffered, defer: true)
         animationBehavior = .alertPanel
         toolbar = .init()
@@ -26,6 +26,112 @@ final class Activity: NSWindow {
         let card = Card()
         content.addSubview(card)
         
+        let eventsTitle = Text(vibrancy: true)
+        eventsTitle.stringValue = "Events"
+        
+        let totalEvents = Text(vibrancy: true)
+        totalEvents.stringValue = "Total events"
+        
+        let events = Text(vibrancy: true)
+        
+        let trackerEvents = Text(vibrancy: true)
+        trackerEvents.stringValue = "Tracker events"
+        
+        let trackers = Text(vibrancy: true)
+        
+        let websitesTitle = Text(vibrancy: true)
+        websitesTitle.stringValue = "Websites"
+        
+        let totalWebsites = Text(vibrancy: true)
+        totalWebsites.stringValue = "Total websites"
+        
+        let websites = Text(vibrancy: true)
+        
+        let mostVisited = Text(vibrancy: true)
+        mostVisited.stringValue = "Most visited"
+        
+        let icon = Icon(size: 24)
+        
+        let visited = Text(vibrancy: true)
+        
+        let trackersTitle = Text(vibrancy: true)
+        trackersTitle.stringValue = "Trackers"
+        
+        let totalTrackers = Text(vibrancy: true)
+        totalTrackers.stringValue = "Total trackers"
+        
+        let prevented = Text(vibrancy: true)
+        
+        let mostPrevented = Text(vibrancy: true)
+        mostPrevented.stringValue = "Most prevented"
+        
+        let most = Text(vibrancy: true)
+        
+        [eventsTitle, websitesTitle, trackersTitle]
+            .forEach {
+                $0.font = .preferredFont(forTextStyle: .title3)
+                $0.textColor = .tertiaryLabelColor
+            }
+        
+        [totalEvents, trackerEvents, totalWebsites, mostVisited, totalTrackers, mostPrevented]
+            .forEach {
+                $0.font = .preferredFont(forTextStyle: .footnote)
+                $0.textColor = .secondaryLabelColor
+            }
+        
+        [events, trackers, websites, prevented, most]
+            .forEach {
+                $0.font = .monospacedSystemFont(ofSize: NSFont.preferredFont(forTextStyle: .body).pointSize, weight: .regular)
+                $0.textColor = .labelColor
+            }
+        
+        [visited, prevented]
+            .forEach {
+                $0.font = .preferredFont(forTextStyle: .body)
+                $0.textColor = .labelColor
+            }
+        
+        let stackEvents = NSStackView(views: [
+            eventsTitle,
+            Separator(mode: .horizontal),
+            totalEvents,
+            events,
+            Separator(mode: .horizontal),
+            trackerEvents,
+            trackers])
+        
+        let stackWebsites = NSStackView(views: [
+            websitesTitle,
+            Separator(mode: .horizontal),
+            totalWebsites,
+            websites,
+            Separator(mode: .horizontal),
+            mostVisited,
+            icon,
+            visited
+        ])
+        
+        let stackTrackers = NSStackView(views: [
+            trackersTitle,
+            Separator(mode: .horizontal),
+            totalTrackers,
+            prevented,
+            Separator(mode: .horizontal),
+            mostPrevented,
+            most])
+        
+        [stackEvents, stackWebsites, stackTrackers]
+            .forEach {
+                $0.translatesAutoresizingMaskIntoConstraints = false
+                $0.orientation = .vertical
+                $0.alignment = .leading
+                content.addSubview($0)
+                
+                $0.topAnchor.constraint(equalTo: card.topAnchor).isActive = true
+                $0.bottomAnchor.constraint(equalTo: card.bottomAnchor).isActive = true
+                $0.widthAnchor.constraint(equalToConstant: 190).isActive = true
+            }
+        
         title.centerYAnchor.constraint(equalTo: content.topAnchor, constant: 26).isActive = true
         title.rightAnchor.constraint(equalTo: content.rightAnchor, constant: -26).isActive = true
         
@@ -33,18 +139,29 @@ final class Activity: NSWindow {
         card.leftAnchor.constraint(equalTo: content.leftAnchor, constant: 30).isActive = true
         card.bottomAnchor.constraint(equalTo: content.bottomAnchor, constant: -30).isActive = true
         
+        stackEvents.rightAnchor.constraint(equalTo: stackWebsites.leftAnchor, constant: -20).isActive = true
+        stackWebsites.rightAnchor.constraint(equalTo: stackTrackers.leftAnchor, constant: -20).isActive = true
+        stackTrackers.rightAnchor.constraint(equalTo: content.rightAnchor, constant: -30).isActive = true
+        
         cloud
-            .map(\.events.since)
-            .removeDuplicates()
-            .map {
-                $0 == nil ? .now : $0!
-            }
-            .sink { since in
+            .map(\.events)
+            .sink {
+                let stats = $0.stats
+                let since = $0.since ?? .now
+                
                 title.attributedStringValue = .init(
                     .init("Activity since ", attributes: .init([
                         .foregroundColor: NSColor.labelColor]))
                     + .init(since.formatted(.relative(presentation: .named, unitsStyle: .wide)), attributes: .init([
                         .foregroundColor: NSColor.secondaryLabelColor])))
+                
+                events.stringValue = stats.websites.formatted()
+                trackers.stringValue = $0.prevented.formatted()
+                websites.stringValue = (stats.domains?.count ?? 0).formatted()
+                icon.icon(icon: stats.domains?.top.lowercased())
+                visited.stringValue = stats.domains?.top ?? ""
+                prevented.stringValue = (stats.trackers?.count ?? 0).formatted()
+                most.stringValue = stats.trackers?.top ?? ""
             }
             .store(in: &subs)
     }
