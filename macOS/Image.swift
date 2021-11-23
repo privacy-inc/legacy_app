b2f2e4542a077bf142ddb4170007da0c74198516
vaux <zero.griffin@gmail.com>
2021-11-23 14:53:01 +0100

    Moves with image

--- macOS/Image.swift
+++ macOS/Image.swift
@@ -1,23 +1,30 @@
 import AppKit
 
 final class Image: NSImageView {
+    private let moves: Bool
+    
     required init?(coder: NSCoder) { nil }
-    convenience init(icon: String) {
-        self.init()
+    convenience init(icon: String, moves: Bool = false) {
+        self.init(moves: moves)
         image = .init(systemSymbolName: icon, accessibilityDescription: nil)
     }
 
-    convenience init(named: String) {
-        self.init()
+    convenience init(named: String, moves: Bool = false) {
+        self.init(moves: moves)
         image = .init(named: named)
     }
     
-    init() {
+    init(moves: Bool = false) {
+        self.moves = moves
         super.init(frame: .zero)
         translatesAutoresizingMaskIntoConstraints = false
     }
     
     override func hitTest(_: NSPoint) -> NSView? {
         nil
+    }
+    
+    override var mouseDownCanMoveWindow: Bool {
+        moves
     }
 }
