128e6862a0f2676caaf2d3f38ab32960e54421b1
vaux <zero.griffin@gmail.com>
2022-04-04 20:09:01 +0200

    Update user agent

--- macOS/Web.swift
+++ macOS/Web.swift
@@ -27,7 +27,7 @@ final class Web: Webview {
         
         super.init(configuration: configuration, settings: settings, dark: NSApp.effectiveAppearance.name != .aqua)
         translatesAutoresizingMaskIntoConstraints = false
-        customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15"
+        customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"
 
         layer = Layer()
         wantsLayer = true
